5bd19928d502ceec45e82f81ad7b1295fcc423ce
leeyounghuncom <web3dao@daum.net>
2024-09-29 14:14:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // 기본 설치 프롬프트 막기
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // 버튼 표시
+  const installBtn = document.getElementById('installBtn');
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    // 설치 프롬프트 표시
+    deferredPrompt.prompt();
+
+    // 사용자의 선택 처리
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('PWA 설치됨');
+      } else {
+        console.log('PWA 설치 거부됨');
+      }
+      deferredPrompt = null; // 초기화
+    });
+  });
+});
+
+/*
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/pwa/service-worker.js')
@@ -9,3 +38,5 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+*/
